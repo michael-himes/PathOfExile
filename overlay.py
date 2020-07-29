b3bb2f40961d6a769987aa1466422995cd1db3bb
michael-himes <37747857+michael-himes@users.noreply.github.com>
2020-07-28 20:10:00 -0600

    Clean up general syntax
    Reduce amount of if statements 
    Make variable names more succinct

--- overlay.py
+++ overlay.py
@@ -7,6 +7,7 @@ import time, sys, pyperclip, json, re, os
 from PySide2.QtWidgets import (QApplication, QLabel, QPushButton, QVBoxLayout, QWidget)
 from PySide2.QtCore import Slot, Qt
 
+
 class MyWidget(QWidget):
     def __init__(self):
         QWidget.__init__(self)
@@ -20,21 +21,18 @@ class MyWidget(QWidget):
         self.setLayout(self.layout)
         # Make window stay on top
         self.setWindowFlags(Qt.WindowStaysOnTopHint)
-        # Git rid of minimize etc.. 
-        # self.setWindowFlags(Qt.FramelessWindowHint)
-
         # Connecting the signal
         self.button1.clicked.connect(self.store)
         self.button2.clicked.connect(self.fill)
 
     @Slot()  
     def fill(self):
-        fill_it()
+        fill_inventory()
     
     def store(self):
-        store_it()
-
-
+        global current_tab
+        current_tab = "Default"
+        store_in_stash()
 
 def click(x_axis, y_axis, times_to_click=1, seconds_after_moving_mouse=.01):
     mouse = Mouse_Controller()
@@ -49,8 +47,7 @@ def on_press(key):
         # Stop listener
         return False
 
-def fill_it():
-    # switch('Default')
+def fill_inventory():
     with Listener( on_press=on_press ) as listener:
         keyboard = Keyboard_Controller()
         keyboard.press(Key.ctrl_l)
@@ -62,18 +59,10 @@ def fill_it():
                 click(x, y)
         keyboard.release(Key.ctrl)
 
-def store_it():
-    time.sleep(.08)
-    switch('Default')
+def store_in_stash():
     global current_tab
-    fragments = [
-        "^Sacrifice at.*",
-        ".*Splinter.*",
-        "Divine Vessel",
-        "Offering to the Goddess",
-        ".*Scarab.*"
-    ]
-    reg_list = [re.compile(x) for x in fragments]
+    switch('Default')
+    time.sleep(.08)
     with Listener( on_press=on_press ) as listener:
         keyboard.press(Key.ctrl)
         i, j = data['Inventory']['Iterator']
@@ -83,58 +72,39 @@ def store_it():
                     break
                 click(x, y, 0)
                 pyperclip.copy('')
-                print(x,y)
                 keyboard.press('c')
                 keyboard.release('c')
                 time.sleep(.01)
-                item = []
                 if len(pyperclip.paste()) > 0:             
                     val = pyperclip.paste()
                     value = val[0:val.find("--------")]
-                    item.append(pyperclip.paste().split('\n')[0])
-                    item.append(pyperclip.paste().split('\n')[1])
-                    # for ike in data["Items"]:
-                    #     if ike in value:
-                    #         print(ike)
-                    if "Map" in value: 
-                        desired_tab("Map")
-                        click(x, y)
-                    elif "Essence" in value:
-                        desired_tab("Essence")
-                        click(x, y)
-                    elif "Card" in value:
-                        desired_tab("Card")
-                        click(x, y)
-                    elif any( item in value for item in data["Items"] ):
-                        # Double loop
-                        save_it = [ item for item in data["Items"] if item in value]
-                    elif any( reg.match(item[1]) for reg in reg_list):
-                        desired_tab("Fragment")
-                        click(x, y)
-                    elif "Currency" in item[0] and "Seed" not in item[1]:
-                        desired_tab("Currency")
-                        click(x, y)
+                    print(value[0:-2])
+                    if any( item in value for item in data["Items"] ):
+                        # Double loop needs a better solution
+                        save_it = [ item for item in data["Items"] if item in value][0]
+                        desired_tab(data["Items"][save_it])
+                        click(x,y)
                     else:
                         desired_tab("Default")
-                        print('yes')
                         click(x, y)
         keyboard.release(Key.ctrl)
 
+def switch(tab):
+    x, y = location(tab)
+    click(x, y)
 
 def desired_tab(tab):
     global current_tab
     if current_tab != tab:
         current_tab = tab 
         switch(tab)
-        print(tab)
         time.sleep(.1)
+    print(tab+"\n")
 
 def location(tab, location="Location"):
     return data[tab][location][1:-1].split(',')
 
-def switch(tab):
-    i = location(tab)
-    click(i[0], i[1])
+
 
 if __name__ == "__main__":
     json_location = os.path.join(os.environ['USERPROFILE'], "Documents\My Games\Path of Exile\locations.json")
@@ -143,7 +113,6 @@ if __name__ == "__main__":
 
     keyboard = Keyboard_Controller()
     mouse = Mouse_Controller()
-    current_tab = "Default"
 
     app = QApplication(sys.argv)
 
